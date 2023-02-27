8314c8f8d03079f821c89cfed474962514375b9d
EduardoTS35 <eduardo.tensan01@gmail.com>
2023-02-27 08:37:06 -0600

    Creación de clase "Gender"

--- HC/Models/Gender.cs
+++ HC/Models/Gender.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HC.Models
+﻿namespace HC.Models
 {
     public class Gender
     {
+        public int Id { get; set; }
+        public string Description { get; set; }
     }
 }
